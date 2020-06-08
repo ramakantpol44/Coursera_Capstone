6d378496b04fc373e0986fdf5b4764f84f6af968
ramakantpol44 <52877847+ramakantpol44@users.noreply.github.com>
2020-06-08 18:33:09 +0530

    Add files via upload

---
+++ MyCapstone.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This Note book is mainly created for Capstone Project
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
